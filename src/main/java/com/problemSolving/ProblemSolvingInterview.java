caf73a0e1184db40511ae05528c76ad64b11a434
preeti64 <58847237+preeti64@users.noreply.github.com>
2024-08-11 12:19:45 +0200

    boiler plate code

---
+++ src/main/java/com/problemSolving/ProblemSolvingInterview.java
@@ -0,0 +1,16 @@
+package com.problemSolving;
+
+import java.util.Arrays;
+
+public class ProblemSolvingInterview {
+    public static void main(String[] args) {
+        int n = 2;
+        int m = 20;
+        int[] result = executeDivisor(n, m);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] executeDivisor(int n, int m){
+        return new int[]{16};
+    }
+}
---
+++ src/test/java/ProblemSolvingInterviewTest.java
@@ -0,0 +1,12 @@
+import com.problemSolving.ProblemSolvingInterview;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ProblemSolvingInterviewTest {
+
+    @Test
+    public void sortAlphanumericString(){
+        Assert.assertEquals(ProblemSolvingInterview.executeDivisor(2,20), new int[]{16});
+    }
+}
+
